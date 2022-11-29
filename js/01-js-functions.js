476244625cf2e405cd28b4c368a3d7c3434a6531
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-11-29 11:57:54 +0530

    js basics

---
+++ js/01-js-functions.js
@@ -0,0 +1,30 @@
+
+// let anyVar; // undefined 
+// console.log(typeof (anyVar));
+// console.log(anyVar);
+
+// anyVar = 10; // number 
+// console.log(typeof (anyVar));
+// console.log(anyVar);
+
+// anyVar = 'abc'; // string 
+// console.log(typeof (anyVar));
+// console.log(anyVar);
+
+
+// const someFun = (arg) => {
+//     let output = arg + 10;
+//     console.log(output)
+// };
+
+// someFun(10);
+
+
+const someFun = (arg) => { // number, string, boolean, array, object, function   
+    let output = arg + 10;
+    console.log(output)
+};
+
+someFun(10);
+
+
---
+++ js/script.js
@@ -0,0 +1,57 @@
+
+// console.log('Hello world!');
+
+// datatypes in JS 
+
+// number, string, boolean 
+
+// NaN undefiled 
+
+// 
+
+
+// num = 10; // don't use 
+// var num2 = 20; // don't use 
+// let num3 = 30; // use for variables 
+// const num4 = 40; // use as your default choice 
+
+
+// let mySalary = 100;
+// console.log(mySalary);
+
+// mySalary = 110;
+// console.log(mySalary);
+
+// functions 
+
+// old JS function 
+
+// function fun() {
+//     console.log('fun function');
+// }
+
+// fun();
+
+// // new js funciton 
+
+// const fun2 = () => {
+//     console.log('fun2 function');
+//     console.log(`asdf`);
+// };
+
+// fun2();
+
+
+const addNums = (i, j) => {
+    console.log(i + j);
+};
+
+addNums();
+addNums(10);
+addNums(10, 20);
+addNums(10, 20, 30);
+
+
+
+
+
