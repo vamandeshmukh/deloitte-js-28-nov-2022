dfe1a96f66e53d5b1249426dd3b97fa58e988a3b
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-11-29 15:37:32 +0530

    node demo

---
+++ deloitte-node-demo/index.js
@@ -0,0 +1,13 @@
+
+const express = require('express');
+const app = express();
+const port = 9090;
+
+app.listen(port, () => {
+    console.log(`Open http://localhost:${port} on your browser.`);
+});
+
+app.get('/', (request, response) => {
+    request.send('Welcome to Deloitte node demo app!');
+});
+
