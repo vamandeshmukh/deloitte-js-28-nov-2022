35bd2929307c8d85c15a05b20012c299a0c55624
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-11-29 12:46:59 +0530

    js basics

--- js/02-js-arrays-objects.js
+++ js/02-js-arrays-objects.js
@@ -1,13 +1,31 @@
 
-const myStuff = [10, 20, 25.5, 'abc', false];
+// const myStuff = [10, 20, 25.5, 'abc', false];
 
-console.log('show array elements using for of loop:');
-for (const elem of myStuff) {
-    console.log(elem);
-}
+// console.log('show array elements using for of loop:');
+// for (const elem of myStuff) {
+//     console.log(elem);
+// }
 
-console.log('show array indices using for in loop:');
-for (const elem in myStuff) {
-    console.log(elem);
-}
+// console.log('show array indices using for in loop:');
+// for (const elem in myStuff) {
+//     console.log(elem);
+// }
 
+
+const empData = {
+    employeeId: 101,
+    firstName: 'Sonu',
+    salary: 10.5,
+    isMarried: false,
+    phones: [9876543210, 6789012345],
+    address: {
+        street: 'Abids',
+        city: 'Hyderabad',
+        pin: 500001
+    },
+    aadhaar: 999911112222,
+    dateOfBirth: '15-Aug-1947'
+};
+
+console.log(empData.firstName);
+console.log(empData.phones[0]);
