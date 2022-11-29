999301366ec6b0fb2e81bea7203a3fdee98f431b
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-11-29 10:42:36 +0530

    js basics

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 
 console.log('Hello world!');
 
+// datatypes in JS 
+
+// number, string, boolean 
+
+// NaN undefiled 
+
+// 
+
+
+num = 10; // don't use 
+var num2 = 20; // don't use 
+let num3 = 30; // use for variables 
+const num4 = 40; // use as your default choice 
+
+
+
+
+
 
